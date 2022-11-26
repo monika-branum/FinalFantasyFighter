10cc2952ad9e8e120b7efda76c428781d473500a
Monika <moncobra@comcast.net>
2022-11-25 19:12:44 -0800

    finally got the displays to work adn increment/decrement

--- app.js
+++ app.js
@@ -2,13 +2,18 @@
 import { renderSoldier } from './render.js';
 
 /* Get DOM Elements */
-const defeatedSoldiers = document.getElementById('defeated-soldiers');
+const defeatedSoldiers = document.getElementById('defeated-number');
 const soldierArray = [];
 const soldierDiv = document.getElementById('soldiers');
 const submitBtn = document.getElementById('submit-button');
 const nameInput = document.getElementById('soldier-name-input');
+const zoroHp = document.getElementById('zoro-hp');
+const soldiersDefeated = document.getElementById('defeated-list');
+const zoroImg = document.getElementById('zoro-img');
+const enemyCounter = document.getElementById('enemy-counter');
 /* State */
-
+let enemiesDefeated = 0;
+let zoroHealth = 20;
 /* Events */
 submitBtn.addEventListener('click', () => {
     createSoldier(nameInput.value, 3);
@@ -20,8 +25,43 @@ function display() {
     soldierDiv.textContent = '';
     for (const item of soldierArray) {
         const soldier = renderSoldier(item);
+        soldier.addEventListener('click', () => {
+            attackHandler(item);
+        });
         soldierDiv.append(soldier);
     }
+    enemyCounter.textContent = `Enemies defeated: ${enemiesDefeated}`;
+}
+
+function attackHandler(soldier) {
+    if (soldier.health <= 0) return;
+    if (zoroHealth <= 0) return;
+    if (Math.random() < 0.33) {
+        soldier.health--;
+        alert(soldier.name + 'Attack landed!' + soldier.name);
+        if (soldier.health === 0) {
+            enemiesDefeated++;
+        }
+    } else {
+        alert('You tried to hit' + soldier.name + 'but your attack missed!');
+    }
+    if (Math.random() < 0.5) {
+        zoroHealth--;
+        alert(soldier.name + 'attacked! The attack hit you!');
+    } else {
+        alert(soldier.name + 'attacked! The attack missed!');
+    }
+    if (zoroHealth === 0) {
+        zoroImg.classList.add('game-over');
+        alert('GAMEOVER');
+    }
+
+    displayZoro();
+    display();
+}
+
+function displayZoro() {
+    zoroHp.textContent = `Zoro HP: ${zoroHealth}`;
 }
 
 function createSoldier(name, health) {
@@ -32,9 +72,14 @@ function createSoldier(name, health) {
     soldierArray.push(newSoldier);
 }
 
+// function enemyCounter {
+//     if ()
+// }
+
 // (don't forget to call any display functions you want to run on page load!)
 createSoldier('Ninjin', 3);
 createSoldier('Tamanengi', 3);
 createSoldier('Piiman', 3);
 
 display();
+displayZoro();
